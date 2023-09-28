2da1779e65efbf7ddcd17e62aca1d869674c46dd
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-09-28 14:10:39 +0330

    feat: implement accordion

---
+++ components/Accordion.tsx
@@ -0,0 +1,42 @@
+import { title } from "process";
+import React, { ReactNode, useState } from "react";
+import { AiFillCaretDown, AiOutlineUp } from "react-icons/ai";
+type AccordionProps = {
+  title: string;
+  children: ReactNode;
+};
+
+const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      className={`w-full bg-stone-950 p-4 rounded-xl  transition-all  ${
+        open && "outline outline-indigo-500"
+      }`}
+    >
+      <div
+        className="text-xl lg:text-2xl w-full flex justify-between items-center lg:cursor-pointer "
+        onClick={() => setOpen(!open)}
+      >
+        <p className={`${open && "text-indigo-400"}`}>{title}</p>
+        <div
+          className={`${
+            !open ? "rotate-180 " : "text-indigo-600"
+          } transition duration-300`}
+        >
+          <AiFillCaretDown />
+        </div>
+      </div>
+
+      <div
+        className={`w-full overflow-hidden max-h-0 transition-all duration-300 ${
+          open ? "max-h-96 overflow-auto pt-4" : ""
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+export default Accordion;
