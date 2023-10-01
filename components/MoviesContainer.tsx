81d7db8f6cdc038a82ed003a6523147f36defea7
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-10-01 17:29:09 +0330

    feat: implement movies container

---
+++ components/MoviesContainer.tsx
@@ -0,0 +1,27 @@
+import { MoviePropTypes } from "@/db";
+import React from "react";
+import MovieBox from "./common/MovieBox";
+
+const MoviesContainer: React.FC<{ data: MoviePropTypes[] }> = ({ data }) => {
+  return (
+    <div className="grid w-full  place-items-center gap-4 lg:grid-cols-2 ">
+      {data.map((item) => {
+        if (!item.top) {
+          return (
+            <MovieBox
+              key={item.id}
+              id={item.id}
+              imageUrl={item.imageUrl}
+              imdbScore={item.imdbScore}
+              movieName={item.movieName}
+              popularity={item.popularity}
+              backgroundImageUrl={item.backgroundImageUrl}
+              description={item.description}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+};
+export default MoviesContainer;
