52fae3bcb2462177a90e31931e0831378fe16567
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-10-01 17:46:39 +0330

    fix bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href='http://fonts.googleapis.com/css?family=Audiowide|Exo+2|Rambla|Scada' rel='stylesheet' type='text/css'/>
         <title>Movie City</title>
       </Head>
       <body className="bg-stone-900">
