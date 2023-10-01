944a57cc6c34ebf02731303b0aac80ce2fbd1277
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-10-01 17:28:00 +0330

    feat: save language to local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 import "@/i18n";
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} /> 
-} 
+  const { i18n, t } = useTranslation();
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem("lang") || "fa";
+    !savedLanguage && localStorage.setItem("lang", "fa");
+    i18n.changeLanguage(savedLanguage);
+  }, []);
+  return <Component {...pageProps} />;
+}
