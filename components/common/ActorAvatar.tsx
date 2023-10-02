623a7f0b2231a54cbfe2f359094bbb0f7bc9f264
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-10-02 16:19:45 +0330

    feat: slice name bigger than 15 charachter

--- components/common/ActorAvatar.tsx
+++ components/common/ActorAvatar.tsx
@@ -7,14 +7,14 @@ interface ActorProps {
 
 const ActorAvatar: React.FC<ActorProps> = ({ imageUrl, name }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-2  text-sm text-white/80 lg:text-base ">
+    <div className="flex flex-col items-center justify-center gap-2  text-center text-sm text-white/80 lg:text-base ">
       <img
         className="h-28 w-28 rounded-full object-cover transition duration-300 hover:opacity-70"
         src={imageUrl}
         alt=""
       />
 
-      <p className="text-sm lg:text-base">{name}</p>
+      <p className="text-xs lg:text-base">{name.length < 15 ? name : name.slice(0,15) + "..."}</p>
     </div>
   );
 };
