88586994c2cbdfb9bb4b0c31267e9a2e1dadf228
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-10-02 16:20:38 +0330

    feat: update comment component border radius

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -10,12 +10,16 @@ interface CommentProps {
 const Comment: React.FC<CommentProps> = ({ sender, text }) => {
   const { t, i18n } = useTranslation();
   return (
-    <div className="flex w-full flex-col items-start justify-start gap-2 rounded-xl bg-neutral-800/80 p-2 text-white/80">
+    <div
+      className={`flex w-full flex-col items-start justify-start gap-2 p-3 ${
+        i18n.dir() === "rtl" ? "rounded-l-full" : "rounded-r-full"
+      }  bg-neutral-800/80 p-2 text-white/80`}
+    >
       <div className="flex items-center justify-center gap-2">
         <div className="rounded-full border-2 border-indigo-500 p-2">
           <FaUserAlt />
         </div>
-        <p>{t("aCommentFrom") + sender}</p>
+        <p className="text-white/60 ">{t("aCommentFrom") + sender}</p>
       </div>
       <p>{text}</p>
     </div>
