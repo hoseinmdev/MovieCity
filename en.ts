bd34e551f518ff6a6a676e928f63ed58c835dcbf
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-10-02 16:17:49 +0330

    feat: add fa and en texts file

---
+++ en.ts
@@ -0,0 +1,113 @@
+export const en = {
+  translation: {
+    noDownloadLink: "No Link for download...",
+    onlineWatch: "Online watch",
+    downloadLinks: "Download Links",
+    quality: "Quality",
+    gb: "Gb",
+    mb: "Mb",
+    size: "Size",
+    iliya: "Iliya",
+    kamran: "Kmaran",
+    sorena: "Sorena",
+    sam: "Sam",
+    greatCommentText: "That was Great !!",
+    goodCommentText: "Good movie... thanks for your good site",
+    soSoCommentText: "Not bad",
+    badCommentText: "I do not like it ...",
+    fantasy: "Fantasy",
+    UnlimitedMoviesTitle: "Ultimated movies, TV shows, and more ...",
+    WatchAnywhereAnytime: "Watch anywhere, anytime...",
+    WatchAnywhereAnytimeDesc:
+      "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV",
+    ReadyToWatch: "Ready to watch !?",
+    GetStarted: "Get Started",
+    EnjoyOnYourTv: "Enjoy on your TV",
+    EnjoyOnYourTvDesc:
+      "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more",
+    ProfileForKids: "Create profiles for kids",
+    ProfileForKidsDesc:
+      "Send kids on adventures with their favorite characters in a space made just for them—free with your membership",
+    DownloadAndSeeOffline: "Download your shows to watch offline",
+    DownloadAndSeeOfflineDesc: "It is free",
+    FrequentlyAskedQuestions: "Frequently Asked Questions",
+    AboutMovieCity: "About Movie City",
+    AboutMovieCityDesc:
+      "Movie City is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want – all for one low monthly price. There is always something new to discover and new TV shows and movies are added every week!",
+    WhereCanIWatch: "Where can i watch?",
+    WhereCanIWatchDesc:
+      "Watch anywhere, anytime including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you are on the go and without an internet connection. Take Movie City with you anywhere.",
+    IsMovieCityGoodForKids: "Is Movie City good for kids?",
+    IsMovieCityGoodForKidsDesc: "Yes, all site content has been reviewed :))) ",
+    RevisedSiteContent: "The content provided in Movie City has been reviewed.",
+    myNumber: "Number : +989222365685",
+    material:
+      "All material and moral rights of this site belong to Hossein Mahmoudi",
+    email: "Email : hoseinmdev@gmail.com",
+    theNun2: "The Nun 2 2023",
+    theNun2Desc:
+      "A priest is murdered. An evil is spreading. The sequel to the worldwide hit follows Sister Irene as she once again encounters Valak, the evil nun.",
+    jawan: "Jawan 2023",
+    jawanDesc:
+      "A common man is punished for a mistake he did not commit. How does the false event affect him and how does he fix it? How did he overcome those who put him in this problem?",
+
+    missionImbossible7: "The movie Mission Impossible 7 - Death Diary",
+    missionImbossible7Desc:
+      " In the film Mission Impossible 7: Ethan Hunt gets a mission from the MIF to prevent a mysterious technology called Entity, which is a powerful artificial intelligence, from falling into the wrong hands. In this way, he encounters enemies that he did not expect...",
+    spiderman: " Spider-Man: Across the Spider-Verse 2023 ",
+    spidermanDesc: `spidermanDesc:"This is the first episode of the two-part sequel to Spider-Man: Into the Spider-Verse (2018) and is set in a shared multiverse of alternate universes called Spider-Man.`,
+
+    ahsoka: "Ahsoka 2023",
+    ahsokaDesc:
+      "After the fall of the Galactic Empire, former Jedi Knight Asuka Tano investigates a new threat to a vulnerable galaxy.",
+    retribution: "Retribution 2023",
+    equalizer: "The Equalizer 3 2023",
+    equalizerDesc:
+      "Robert McCall finds himself at home in southern Italy, only to discover that his friends are under the control of local crime bosses. As events turn deadly, McCall knows what he must do: protect his friends by taking on the mafia.",
+    oppenheimer: "Oppenheimer 2023",
+    oppenheimerDesc: `In the movie Oppenheimer: The story of this movie, focusing on Julius Robert Oppenheimer, deals with the story of a group of Manhattan Project scientists who produced the atomic bombs used in the cities of Hiroshima and Nagasaki in Japan for the US Army. Surrender of the Japanese Empire after the atomic bombing ended the Pacific War. Nolan's new work is based on the book "American Prometheus: The Triumph and Tragedy of J. Robert Oppenheimer" by Kai Byrd and the late Martin J. Sherwin.`,
+
+    meg2: "The Meg 2 2023",
+    meg2Desc:
+      "In the movie Mag 2 Pit: Jonas Taylor is researching a young megalodon (extinct shark) that is kept in captivity due to the lack of its generation. This rare shark was found in a place called the Mariana Trench, where many people are looking for more information in this field and...",
+
+    transformers: "Transformers 7 2023",
+    transformersDesc:
+      "The story of Transformers: Rise of the Beasts is about a flawed and vulnerable character who has made mistakes in his life and now decides to make amends by helping Optimus Prime.",
+    littleMermaidDesc:
+      "Ariel, a young mermaid, trades a sea witch: his beautiful voice for human feet so he can explore the world above water.",
+    littleMermaid: "Little Mermaid 2023",
+    elmental: "Elmental 2023",
+    elmentalDesc:
+      "Elemental Animation follows Ember and Wade, in a city where the inhabitants of fire, water, earth and air live together.",
+    flash: "Flash 2023",
+    flashDesc:
+      "In The Flash, Barry Allen travels back in time to save his mother's life, and his work causes unwanted changes in the timeline...",
+    exteraction2: "Exteraction 2 2023",
+    exteraction2Desc:
+      "After being presumed dead in the first film, Black Ops mercenary Tyler Rock returns for another dangerous mission...",
+    coldHearted: "Cold Hearted 2023",
+    coldHeartedDesc:
+      "Rachel Stone is an intelligence agent for an international agency who must embark on a dangerous mission to protect a mysterious weapon known as the Heart. Now, Rachel Stone has the duty to protect this weapon from falling into the hands of the enemy and...",
+    topMovie: "Top Movie",
+    topContent: "Top Content",
+    download: "Download",
+    comingSoon: "Coming Soon",
+    lastestMovies: "Latest Movies",
+    joker2: "Joker 2 2023",
+    generalV: "Gen V 2023",
+    aHuntingInVenice: "a Hunting In Venice 2023",
+    sawX: "Saw X 2023",
+    promisedLand: "Promised Land 2023",
+    reptile: "Reptile 2023",
+    marvels: "Marvels 2023",
+    director: "Director",
+    actors: "Actors : ",
+    action: "Action",
+    scary: "Scary",
+    biography: "Biography",
+    trailer: "Trailer",
+    aCommentFrom: "A comment from ",
+    usersComments: "Users Comments",
+  },
+};
