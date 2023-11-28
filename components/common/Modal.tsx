667b2e26030b581682edaedfb83d5c453bfddfe7
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-11-28 13:50:53 +0330

    change bg color brightness

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -6,7 +6,7 @@ type ModalProps = {
 
 const Modal: React.FC<ModalProps> = ({ children }) => {
   return (
-    <div className="absolute  left-0 right-0 lg:top-[8.5rem] top-[7.6rem] z-[1000] ml-auto mr-auto h-[86vh] w-[95%] lg:w-full rounded-lg bg-black/70 text-white backdrop-blur-sm fadeShow">
+    <div className="fixed  left-0 right-0 lg:top-[8.5rem] top-[7.6rem] z-[1000] ml-auto mr-auto h-[86vh] w-[95%] lg:w-full rounded-lg bg-black/60 lg:bg-black/70 text-white backdrop-blur-sm fadeShow">
       {children}
     </div>
   );
