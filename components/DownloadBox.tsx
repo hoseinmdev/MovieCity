766e93706fe277bf52cdce5d443d9cf4b9490e70
Hosein Mahmoudi <hoseinmdev@gmail.com>
2023-11-28 13:50:40 +0330

    add modal for download box

--- components/DownloadBox.tsx
+++ components/DownloadBox.tsx
@@ -1,17 +1,35 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import Modal from "./common/Modal";
+import { IoMdClose } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineVerifiedUser } from "react-icons/md";
+import Image from "next/image";
 
 interface DownloadBoxProps {
-  movieId:string,
+  movieId: string;
   quality: number;
   size: number;
   link: string;
 }
 
-const DownloadBox: React.FC<DownloadBoxProps> = ({ size, link, quality,movieId }) => {
+const DownloadBox: React.FC<DownloadBoxProps> = ({
+  size,
+  link,
+  quality,
+  movieId,
+}) => {
   const { t, i18n } = useTranslation();
-
+  const [isLogin, setIsLogin] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token") || "";
+    if (token) setIsLogin(true);
+  }, []);
+  const isLoginHandler = () => {
+    if (!isLogin) setShowModal(true)
+  }
   return (
     <div className="flex w-full items-center justify-between rounded-lg bg-neutral-800 p-2 text-sm text-white/80 lg:p-4 lg:text-base">
       <div className=" flex h-full flex-col justify-center gap-4 lg:gap-2">
@@ -34,12 +52,47 @@ const DownloadBox: React.FC<DownloadBoxProps> = ({ size, link, quality,movieId }
       </div>
       <div className="flex flex-col items-end gap-2 lg:flex-row ">
         <a
-          href={link}
+          href={isLogin ? link : undefined}
+          onClick={isLoginHandler}
           className="flex h-full w-40 justify-center rounded-lg bg-indigo-500 p-2 lg:p-3"
         >
           {t("download") + " " + t("quality") + " " + quality}
         </a>
       </div>
+      {showModal && (
+        <Modal>
+          <div className="flex h-full w-full flex-col items-center justify-start gap-6 p-4 text-white">
+            <div
+              onClick={() => setShowModal(false)}
+              className="self-start text-2xl text-red-500"
+            >
+              <IoMdClose />
+            </div>
+            <div className="flex flex-col gap-6">
+              <p className="text-lg">{t("dearUser") + " 👑"}</p>
+              <p className="whitespace-nowrap">{t("forDownloadMovie")}</p>
+              <p className="">{t("note")}</p>
+            </div>
+            <div className="mt-8 flex w-full flex-col items-center justify-between gap-2">
+              <Link href={"/signup"} className="flex w-full  items-center justify-center gap-2 rounded-lg bg-red-600 p-2">
+                {t("signUp")}
+                <div className="text-lg text-white">
+                  <MdOutlineVerifiedUser />
+                </div>
+              </Link>
+              <button
+                onClick={() => setShowModal(false)}
+                className="flex w-full  items-center justify-center gap-2 rounded-lg border border-red-500 bg-none p-2"
+              >
+                {t("later")}
+                <div className="text-lg text-white">
+                  <IoMdClose />
+                </div>
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
