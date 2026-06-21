293859ff7ec97492018d4ef3a8899049e2b43cff
Mobin <hoseinmdev@gmail.com>
2026-06-21 19:51:42 +0330

    fix: use static height for main slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -134,7 +134,7 @@ const Slider: React.FC<SliderProps> = ({ movies }) => {
         pauseOnMouseEnter: true,
         disableOnInteraction: false,
       }}
-      className={`group/trackBox mySwiper fadeShow mb-4 mt-2 flex h-[20rem] w-full cursor-pointer items-center justify-center overflow-hidden rounded-b-2xl transition-all sm:h-[24rem] md:h-[28rem] lg:h-[32rem] lg:max-h-none lg:min-h-max 2xl:h-[40rem] ${
+      className={`group/trackBox mySwiper fadeShow mb-4 mt-2 flex h-[32rem] w-full cursor-pointer items-center justify-center overflow-hidden rounded-b-2xl ${
         i18n.dir() === "rtl" ? "font-VazirFont" : "font-BlackListed"
       }`}
     >
